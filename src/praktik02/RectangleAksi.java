64862c5a68413e3719c4867d63e576f09e7e8b45
ekaerlina <eka.erlinaa11@gmail.com>
2022-12-16 13:07:37 +0800

    Mengisi Nilai Atribut

--- src/praktik02/RectangleAksi.java
+++ src/praktik02/RectangleAksi.java
@@ -3,5 +3,7 @@ package praktik02;
 public class RectangleAksi {
     public static void main(String[] args){
         Rectangle rl = new Rectangle();
+        rl.panjang = 9;
+        rl.lebar = 3;
     }
 }
